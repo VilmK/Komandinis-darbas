c6aa4287e732cc64a7c7c711cb2652f0d3eaf5ab
VilmK <pipeika@gmail.com>
2021-10-29 19:24:32 +0300

    Sudėliota mygtukų funkcija

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+let buttonDarzoves = document.querySelector("#darzoves");
+buttonDarzoves.addEventListener("click", function () {
+    let vais = document.querySelector(".vaisiai");
+    vais.classList.add("d-none");
+})
+
+let buttonVaisiai = document.querySelector("#vaisiai");
+buttonVaisiai.addEventListener("click", function () {
+    let darz = document.querySelector(".darzove");
+    darz.classList.add("d-none");
+})
+
+let buttonVisi = document.querySelector("#visi");
+buttonVisi.addEventListener("click", function () {
+    let darz = document.querySelector(".darzove");
+    let vais = document.querySelector(".vaisiai");
+    vais.classList.remove("d-none");
+    darz.classList.remove("d-none");
+})
+
+
+
