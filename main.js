55fb784f66f14dd727b9f46a3673cc663ee93249
VilmK <pipeika@gmail.com>
2021-10-29 20:24:22 +0300

    koreguota js

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ let buttonDarzoves = document.querySelector("#darzoves");
 buttonDarzoves.addEventListener("click", function () {
     vaisiai.classList.add("d-none");
 })
+
 let buttonVaisiai = document.querySelector("#vaisiai");
 buttonVaisiai.addEventListener("click", function () {
     darzove.classList.add("d-none");
 })
+
 let buttonVisi = document.querySelector("#visi");
 buttonVisi.addEventListener("click", function () {
     vaisiai.classList.remove("d-none");
