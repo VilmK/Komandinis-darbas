4bbf37fb392773cd81ab2ad1912731210ace8193
VilmK <93336731+VilmK@users.noreply.github.com>
2021-10-29 20:29:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
+let darzoves = document.querySelector(".darzove");
+let vaisiai = document.querySelector(".vaisiai");
 
 let buttonDarzoves = document.querySelector("#darzoves");
 buttonDarzoves.addEventListener("click", function () {
-    let vais = document.querySelector(".vaisiai");
-    vais.classList.add("d-none");
+    vaisiai.classList.add("d-none");
 })
 
 let buttonVaisiai = document.querySelector("#vaisiai");
 buttonVaisiai.addEventListener("click", function () {
-    let darz = document.querySelector(".darzove");
-    darz.classList.add("d-none");
+    darzoves.classList.add("d-none");
 })
 
 let buttonVisi = document.querySelector("#visi");
 buttonVisi.addEventListener("click", function () {
-    let darz = document.querySelector(".darzove");
-    let vais = document.querySelector(".vaisiai");
-    vais.classList.remove("d-none");
-    darz.classList.remove("d-none");
+    vaisiai.classList.remove("d-none");
+    darzoves.classList.remove("d-none");
 })
 
 
